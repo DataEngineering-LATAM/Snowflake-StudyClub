790d0f91802e0b4c0713dd5ed04b560d27556226
aespinosa1983 <113540010+aespinosa1983@users.noreply.github.com>
2022-11-10 11:15:23 -0600

    Create ClassicWeb_Lab.sql

---
+++ Scripts/Sesion 10/ClassicWeb_Lab.sql
@@ -0,0 +1,11 @@
+// 1.- Crear base de datos en Worksheet
+Create database COMUNIDAD;
+
+// 2.- Crear tabla en Database
+// Create Table comunidad  con campos Nombre y tipo
+
+// 3.- Insertar datos en la tabla comunidad en el worksheet
+Insert Into COMUNIDAD (nombre, tipo) values ('Delatam', 'Data Engineering');
+
+// 4.- Consultar Comunidad en worksheet
+Select * from Usuarios;
